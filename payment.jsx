39c2635ee3daeb7f1e2234a7c6d131a645b79cc0
saamarthjohn02 <saamarthjohn02@gmail.com>
2026-07-15 11:10:55 +0530

    update6

---
+++ payment.jsx
@@ -0,0 +1,10 @@
+import axios from 'axios'
+function Payment(){
+    async function javascript(src) {
+        return new Promise((resolve)=> {
+            const script=document.createElement("")
+            
+        })
+        
+    }
+}
